5a5a183cc0c016fded46e88432bbbaf5d7b2ce8d
MFADIGA3 <MFADIGA3@ford.com>
2021-12-20 17:44:56 -0300

    feat(heroes.component): added a list of heroes, user can see details of selected heroe
    used *ngFor to create the list, *ngIf to conditionally exclude a blobk of HTML and toggle css using class binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../models/hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,17 @@ import { Hero } from '../models/hero.model';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
+  onSelect(hero: Hero): void {
+
+    this.selectedHero = hero;
   }
 }
