69ea5c9abb0eff7049ea83ebfb88640fb5eacff9
MFADIGA3 <MFADIGA3@ford.com>
2021-12-28 17:20:00 -0300

    feat(dashboard): added images on dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,6 +12,9 @@ export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
   topHero?: Hero;
+  cleanImage?: string;
+  cleanName?: string
+  haveImage?: boolean
 
   constructor(private heroService: HeroService) { }
 
@@ -24,7 +27,36 @@ export class DashboardComponent implements OnInit {
       .subscribe(heroes => {
         this.heroes = heroes.slice(1, 5);
         this.topHero = heroes[0];
+        this.cleanImage = this.transform(heroes[0].image);
+        this.cleanName = this.getLastName(heroes[0].name);
+        if(this.cleanImage === this.cleanName) {
+          this.haveImage = true
+        } else {
+          this.haveImage = false;
+        }
       })
+  }
+
+  teste(imageParam: string, nameParam: string): void {
+
+    const CLEAN_IMAGE = this.transform(imageParam);
+    const CLEAN_NAME = this.getLastName(nameParam);
+    if (CLEAN_IMAGE === CLEAN_NAME) {
+      console.log(true);
+    }
+  }
+
+  getLastName(fullName: string) {
+    const LAST_NAME_ARR = fullName.split(" ");
+    let lastName = LAST_NAME_ARR[LAST_NAME_ARR.length - 1]
+    lastName = lastName.toLowerCase();
+    return lastName;
+  }
+
+  transform(imageUrl: string): string {
+    const urlArray = imageUrl.split("/");
+    const name = urlArray[urlArray.length - 1].split(".")[0];
+    return name;
   }
 
 }
