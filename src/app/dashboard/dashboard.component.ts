c1c442d34477030840a6162f765b36a3a2f51df6
MFADIGA3 <MFADIGA3@ford.com>
2021-12-29 18:00:00 -0300

    style(dashboard): add color dinamically to elements
    First, Second and Third heroes have specialcolor on border and id. Added colors dinamically to 'hero.name' depending on 'hero.hairColor'

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,13 +10,10 @@ import { HeroService } from '../services/hero.service';
 
 export class DashboardComponent implements OnInit {
 
-  color!: string;
+  colors = [{ color: "" }];
   allHeroes: Hero[] = [];
   heroes: Hero[] = [];
   topHero!: Hero;
-  cleanImage?: string;
-  cleanName?: string
-  haveImage?: boolean
 
   constructor(private heroService: HeroService) { }
 
@@ -24,24 +21,60 @@ export class DashboardComponent implements OnInit {
     this.getHeroes();
   }
 
-  ngDoCheck(): void {
-    console.log(this.heroes)
-    // this.checkColor(this.color)
-    // console.log(this.color)
-  }
+  // ngDoCheck(): void {
+  //   console.log(this.heroes)
+
+  // }
 
   getHeroes(): void {
+
     this.heroService.getHeroes()
       .subscribe(heroes => {
         this.allHeroes = heroes;
         this.heroes = heroes.slice(1, 5);
         this.topHero = heroes[0];
-        this.color = 'silver';
+        this.checkColor(this.heroes);
       })
   }
 
-  checkColor(color: string): void {
+  checkColor(heroes: Hero[]): void {
 
+    heroes.forEach((hero, i) => {
+      let haveMultipleColors = hero.hairColor.search(/[,]/gi)
+      if (haveMultipleColors !== -1) {
+        const AUX_ARR = hero.hairColor.split(", ");
+        this.colors[i] = { 'color': this.parseColor(AUX_ARR[0]) }
+        // AUX_ARR.forEach((color, j) => {
+        //   console.log(color);
+        //   this.colors[i] = { [j]: color };
+        // })
+      }
+      else{
+        this.colors[i] = { 'color': this.parseColor(hero.hairColor) };
+      }
+
+      // this.colors[i] = {this.parseColor(hero.hairColor)};
+      console.log(this.colors[i].color);
+    })
+
+    console.log(this.colors);
+  }
+
+  parseColor(color: string) {
+    let parsedColor = color.replace(/[ ,]/gi, "").toLowerCase();
+
+    return parsedColor
   }
 
 }
+
+
+colors: [
+  {
+    '0': "colorName"
+  },
+  {
+    '0': "colorName",
+    '1': "colorName"
+  }
+]
