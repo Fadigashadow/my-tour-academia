2d36c191d4e9f1051b5860db767f3c94e26ccd6f
MFADIGA3 <MFADIGA3@ford.com>
2021-12-30 21:51:15 -0300

    feat(dashboard): add hero-list to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,7 +10,6 @@ import { HeroService } from '../services/hero.service';
 
 export class DashboardComponent implements OnInit {
 
-  colors = [{ color: "" }];
   allHeroes: Hero[] = [];
   heroes: Hero[] = [];
   topHero!: Hero;
@@ -28,37 +27,6 @@ export class DashboardComponent implements OnInit {
         this.allHeroes = heroes;
         this.heroes = heroes.slice(1, 5);
         this.topHero = heroes[0];
-        this.checkColor(this.heroes);
       })
   }
-
-  checkColor(heroes: Hero[]): void {
-
-    heroes.forEach((hero, i) => {
-      let haveMultipleColors = hero.hairColor.search(/[,]/gi)
-      if (haveMultipleColors !== -1) {
-        const AUX_ARR = hero.hairColor.split(", ");
-        this.colors[i] = { 'color': this.parseColor(AUX_ARR[0]) }
-        // AUX_ARR.forEach((color, j) => {
-        //   console.log(color);
-        //   this.colors[i] = { [j]: color };
-        // })
-      }
-      else {
-        this.colors[i] = { 'color': this.parseColor(hero.hairColor) };
-      }
-
-      // this.colors[i] = {this.parseColor(hero.hairColor)};
-      console.log(this.colors[i].color);
-    })
-
-    console.log(this.colors);
-  }
-
-  parseColor(color: string) {
-    let parsedColor = color.replace(/[ ,]/gi, "").toLowerCase();
-
-    return parsedColor
-  }
-
 }
